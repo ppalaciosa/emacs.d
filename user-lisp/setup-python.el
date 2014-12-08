a48d6605c9f7ebb13df07d57d1bd7df5e6bce7e2
Pedro Palacios Avila <pedro_gpa@hotmail.com>
2014-12-08 16:35:41 -0500

    Ordenada configuracion de python

--- user-lisp/setup-python.el
+++ user-lisp/setup-python.el
@@ -2,8 +2,14 @@
 ;; autocompletado) es necesario instalar un ambiente virtual
 ;; de python (virtualnv) en .python-environments y ejecutar
 ;; M-x jedi:install-server
+;; jedi mostrara documentacion de los modulos instalados en el
+;; ambiente virtual .python-enviroments
 
-(add-hook 'python-mode-hook 'jedi:setup)
+(add-hook 'python-mode-hook (lambda ()
+                              (jedi:setup)
+                              ;; Para evitar que  C-tab sea asignado a otra funcion
+                              (define-key jedi-mode-map (kbd "<C-tab>") nil)
+                              ))
 (setq jedi:complete-on-dot t)
 (setq jedi:environment-root
       (expand-file-name ".python-environments" user-emacs-directory))
